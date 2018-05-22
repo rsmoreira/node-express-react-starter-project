1a948cd932b57d4dfd2691af0d7f24e62eacdf82
rsmoreira <rsmoreira.mail@gmail.com>
2018-05-21 21:19:59 -0300

    Login/Logout routers added to FrontEnd.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,9 +5,9 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>Survey New</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
     componentDidMount() {
@@ -21,7 +21,7 @@ class App extends Component {
                     <div>
                         <Header />
                         <Route exact path="/" component={ Landing } />
-                        <Route exact path="/surveys" component={ Dashboard } />
+                        <Route exact path="/dashboard" component={ Dashboard } />
                         <Route exact path="/surveys/new" component={ SurveyNew } />
                     </div>
                 </BrowserRouter>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+    
     render() {
         return(
             <nav>
                 <div className="nav-wrapper">
-                
-                    <a href="/" className="left brand-logo">e-Core Native App</a>
-                
+                    <Link 
+                        to={this.props.auth ? "/dashboard" : "/"}
+                        className="left brand-logo"
+                    >
+                        e-Core Native
+                    </Link>
+                    
                     <ul id="nav-mobile" className="right">
-                        <li><a href="/auth/google">Login With Google</a></li>
+                        {this.renderContent()}
                     </ul>
                 
                 </div>
@@ -18,4 +35,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
