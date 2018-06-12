2a003343bdde721172b639db419af472dec5ee1a
rsmoreira <rsmoreira.mail@gmail.com>
2018-06-11 22:49:47 -0300

    Code refactoring to add ldap credentials as keys.

--- config/keys/prd.js
+++ config/keys/prd.js
@@ -2,5 +2,10 @@ module.exports = {
     googleClientID : process.env.GOOGLE_CLIENT_ID,
     googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
     mongoURI : process.env.MONGO_URI,
-    cookieKey : process.env.COOKIE_KEY 
+    cookieKey : process.env.COOKIE_KEY,
+    ldapUrl: process.env.LDAP_URL,
+    ldapBindDN: process.env.LDAP_BIND_DN,
+    ldapBindCredentials: process.env.LDAP_BIND_CREDENTIALS,
+    ldapSearchBase: process.env.LDAP_BASE_SEARCH,
+    ldapSearchFilter: process.env.LDAP_SEARCH_FILTER
 };
